150b18fc2d0b0c1872b98c42cb3efe990d0c96bd
Guest User <aleah.luk@gmail.com>
2016-06-14 13:02:39 +0800

    created interactive interface with countdown timer

--- cookies_and_oven.rb
+++ cookies_and_oven.rb
@@ -5,3 +5,49 @@
 # - What interface will each class provide?
 # - How will the classes interact with each other?
 # - Which classes will inherit from others, if any?
+
+
+class Cookies
+	attr_accessor :minutes, :type
+
+	def initialize(type, duration)
+		@type = type
+		@duration = duration.to_i
+		@minutes = 0
+		@time = @duration + 1
+	end
+	def time_down
+		until @minutes == @duration
+			puts "The cookies are done!" if self.cooked? == true
+			puts "Oh no! The cookies are burnt!" if self.burnt? == true
+			@minutes += 1
+			@time -= 1
+			output = "#{@time} minutes left." + "\r"
+			print output
+			$stdout.flush
+			sleep(1)
+		end
+	end
+	def cooked?
+		if @type == "chocolate"
+			return true if @minutes == 20
+		elsif @type == "butterscotch"
+			return true if @minutes == 15
+		end
+	end
+	def burnt?
+		return true if @minutes == 30
+	end
+	def bake
+		puts "The cookies are in the oven and doughy."
+		self.time_down
+	end
+end
+
+puts "What type of cookies would you like to cook? (chocolate/butterscotch)"
+type = gets.chomp
+puts "How many minutes do you want to bake it for?"
+duration = gets.chomp.to_i
+puts ""
+cookies = Cookies.new(type, duration)
+cookies.bake
